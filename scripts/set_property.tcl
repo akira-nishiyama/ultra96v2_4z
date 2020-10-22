95e897f38382e39739a4bd2af6410b617e70e554
Akira Nishiyama <mr081677@gmail.com>
2020-10-22 22:39:46 +0900

    first commit

---
+++ scripts/set_property.tcl
@@ -0,0 +1,9 @@
+#set board
+set_property board_part em.avnet.com:ultra96v2:part0:1.0 [current_project]
+
+#set property for embedded acceleration platform
+set_property platform.design_intent.embedded true [current_project]
+set_property platform.design_intent.server_managed false [current_project]
+set_property platform.design_intent.external_host false [current_project]
+set_property platform.design_intent.datacenter false [current_project]
+set_property platform.default_output_type "sd_card" [current_project]
